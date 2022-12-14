12d706b2419e190b3c695db998f6d1faea8f17ce
cristopxer <cristopherortiz457@gmail.com>
2022-12-14 17:46:02 -0600

    User Widget completed

--- src/scenes/homePage/homePage.jsx
+++ src/scenes/homePage/homePage.jsx
@@ -1,10 +1,27 @@
 import Navbar from "scenes/navbar/navbar";
 import { Box } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import UserWidget from "scenes/widgets/UserWidget";
 
 function HomePage() {
+  const isNonMobileScreens = useMediaQuery("(min-width: 600px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+
   return (
     <Box>
       <Navbar />
+      <Box
+        width="100%"
+        p="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+      </Box>
     </Box>
   );
 }
